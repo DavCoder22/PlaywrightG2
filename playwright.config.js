28444eb6da18e34df3dc8eeedb276300633ba7a4
DavCoder22 <87149195+DavCoder22@users.noreply.github.com>
2026-05-19 21:39:14 -0500

    Use BASE_URL secret for dynamic URL

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: 'https://taskflow-sooty-pi.vercel.app',
+    baseURL: process.env.BASE_URL || 'https://taskflow-sooty-pi.vercel.app',
     trace: 'on-first-retry',
   },
   projects: [
